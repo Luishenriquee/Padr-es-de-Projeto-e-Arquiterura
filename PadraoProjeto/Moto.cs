13733b71d31e01d5e40f00b4197a3f00a91488a1
Luis Henrique <iluishenriquebr@gmail.com>
2022-01-11 12:30:46 -0300

    Adição da classe "Moto" fazendo uma relação de herança com a classe "Veiculo" e, executando a sobrescrita do método abstrato "getTipo" com a palavra override.

---
+++ PadraoProjeto/Moto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PadraoProjeto
+{
+    public class Moto : Veiculo
+    {
+        public override void getTipo()
+        {
+            Console.WriteLine("Veículo do tipo Moto");
+        }
+    }
+}
