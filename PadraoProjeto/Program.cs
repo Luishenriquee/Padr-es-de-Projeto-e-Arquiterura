cad144cf36652dacba93f5ce74ec6ef4a77cd3b1
Luis Henrique <iluishenriquebr@gmail.com>
2022-01-11 13:32:40 -0300

    Os códigos a seguir demonstram a criação de objetos do tipo Creator para cada tipo de produto possibilitando a criação de uma fábrica de objetos do tipo Moto e outra do tipo Carro. Essas implementações estão na classe Program que está fazendo o papel de cliente que irá utilizar das fábricas.

---
+++ PadraoProjeto/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace PadraoProjeto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Creator fabricaCarro = new CarroCreator();
+            Creator fabricaMoto = new MotoCreator();
+
+            Carro meuCarro = (Carro)fabricaCarro.criarVeiculo();
+            Moto minhaMoto = (Moto)fabricaMoto.criarVeiculo();
+
+            meuCarro.getTipo();
+            minhaMoto.getTipo();
+
+        }
+    }
+}
