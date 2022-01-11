92dddda36c1bd8b86df49b77601091f3429d4ef9
Luis Henrique <iluishenriquebr@gmail.com>
2022-01-11 12:28:12 -0300

    Adição da classe "Carro" fazendo uma relação de herança com a classe "Veiculo" e, executando a sobrescrita do método abstrato "getTipo" com a palavra override.

---
+++ PadraoProjeto/Carro.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PadraoProjeto
+{
+    public class Carro : Veiculo
+    {
+        public override void getTipo()
+        {
+            Console.WriteLine("Veículo do tipo Carro");
+        }
+    }
+}
